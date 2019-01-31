86517b8c5d11faf805cfb0c61a0a91a8b808fb2f
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-31 11:07:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function loadIssues() {
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
-  for(var i=0;i<issues.length;i++) {
-    var result = template(issues[i]);
+var result = template(issues);
     document.getElementsByTagName("main")[0].innerHTML += result;
   }
 }
