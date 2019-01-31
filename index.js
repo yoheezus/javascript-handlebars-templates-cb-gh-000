a6ac0e1d70414405b42a521cfe806e7f04e1c918
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-31 11:06:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function loadIssue() {
+function loadIssues() {
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
   for(var i=0;i<issues.length;i++) {
     var result = template(issues[i]);
